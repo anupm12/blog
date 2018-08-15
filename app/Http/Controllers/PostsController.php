c22b59e247c10ca75d8439897144a0e0cf9ea69e
anupam shukla <shuklaanupam18@gmail.com>
2018-08-15 18:27:03 +0530

    data validation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -34,7 +34,13 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
+
+        $this->validate($request, [
+            'title' => 'required',
+            'featured' => 'required|image',
+            'content' => 'required'
+        ]);
     }
 
     /**
